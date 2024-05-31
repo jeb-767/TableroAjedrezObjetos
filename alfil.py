c8de7c37c8c0ccd5f1827c74e857aea4f871d7c4
Joan <65063315+jeb-767@users.noreply.github.com>
2024-05-31 08:54:29 +0200

    Update alfil.py

--- alfil.py
+++ alfil.py
@@ -1,7 +1,8 @@
 class Alfil:
-    def __init__(self, posicion, nombre):
+    def __init__(self, posicion, nombre, color):
         self.posicion = posicion
         self.nombre = nombre
+        self.color = color
 
     def __str__(self):
         return self.nombre
@@ -13,32 +14,28 @@ class Alfil:
         self.tablero = tablero
 
     def movimiento_legal(self, posicion_final):
+        fila = self.posicion[1]
         legal = True
-        if self.posicion[0] == posicion_final[0]:
-            if self.posicion[1] > posicion_final[1]:
-                numero = -1
-            else:
-                numero = 1
-            for a in range(self.posicion[1], posicion_final[1], numero):
-                A = self.tablero[a][posicion_final[0]]
-                if A != "" and A != self.tablero[self.posicion[1]][self.posicion[0]]:
-                    legal = False
-                    break
-        elif self.posicion[1] == posicion_final[1]:
-            if self.posicion[0] < posicion_final[0]:
-                numero = 1
-            else:
-                numero = -1
-            for F in range(self.posicion[0], posicion_final[0], numero):
-                A = self.tablero[posicion_final[1]][F]
-                if A != "" and A != self.tablero[self.posicion[1]][self.posicion[0]]:
-                    legal = False
-                    break
-        elif self.posicion[0] != posicion_final[0] or self.posicion[1] != posicion_final[1]:
-            return False
+        if movimiento[0] > movimiento[2]:
+            contador = -1
+        else:
+            contador = 1
+        if movimiento[1] > movimiento[3]:
+            contador2 = -1
+        else:
+            contador2 = 1
+        for a in range(movimiento[0], movimiento[2], contador):
+            A = m[fila][a]
+            if A != "" and A != x:
+                legal = False
+                break
+            fila += contador2
         if legal == False:
+            print("El movimiento no es legal")
             return False
         else:
+            print("El movimiento es legal")
+            self.mover(movimiento)
             return True
 
     def mover(self, movimineto):
